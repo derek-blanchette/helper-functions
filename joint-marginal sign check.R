47694b7240d70fb47bbadf4f15a8b363851034c7
Derek R. Blanchette <drblanchette@gmail.com>
2022-05-25 21:15:13 -0500

    add optional decimals argument for rounding

--- joint-marginal sign check.R
+++ joint-marginal sign check.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(broom)
 
-reality.check = function(var.list, y, df){
+reality.check = function(var.list, y, df, decimals=6){
   
   f = reformulate(var.list, response = y)
   model.full = glm(f, data = df, family = "binomial")
@@ -23,7 +23,7 @@ reality.check = function(var.list, y, df){
   colnames(results) = c("term","MEstimate", "MSE", "MZ", "MPVal")
   
   final = merge(tidy.full, results, by.x = "term", by.y="term")
-  final[,2:9] = round(final[,2:9],6)
+  final[,2:9] = round(final[,2:9], decimals)
   final$CHECK = ifelse(sign(final$JEstimate) == sign(final$MEstimate), "PASS","FAIL")
   return(final)
 }
